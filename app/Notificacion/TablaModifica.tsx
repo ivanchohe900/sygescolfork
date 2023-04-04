497c9c9f7061e0ad5f535c665bad6f1dc81c5ed3
ivanchohe90 <ivanchohe90@gmail.com>
2023-04-04 10:27:44 -0500

    Cambios en novedad docente

--- app/Notificacion/TablaModifica.tsx
+++ app/Notificacion/TablaModifica.tsx
@@ -2,41 +2,21 @@ import React from "react";
 
 const tableData = [
   {
-    preescolar: "Sin Asignacion",
-    primaria: "Sin Asignacion",
-    secundaria:
-      "6-01, 6-02, 6-03, 7-01, 7-02, 7-03, 7-04, 8-01, 8-02, 8-03, 6-01, 6-02, 6-03, 7-01, 7-02, 7-03, 7-04, 8-01, 8-02, 8-03,",
-    media: "11-01",
-    estado: "Aprobado",
+    docente: "Brahian Orozco",
+    Asignatura: "Humanidades",
+    Grado: "6-01",
+    TipoP: "No se ha subido Comportamiento",
   },
 ];
 
 const tableRows = tableData.map((data, index) => (
   <tr key={index} className="bg-blue-500 border-b border-blue-400">
     <th scope="row" className="px-6 py-4 text-white">
-      {data.preescolar}
+      {data.docente}
     </th>
-    <td className="px-6 py-4 text-white font-semibold">{data.primaria}</td>
-    <td className="px-6 py-4 text-white font-semibold">{data.secundaria}</td>
-    <td className="px-6 py-4 text-white font-semibold">{data.media}</td>
-
-    <td className="px-6 py-4 text-black">
-      <input
-        className=" p-1 rounded-full border-solid border-2 border-indigo-600"
-        type="datetime-local"
-        name="datetime"
-      />
-    </td>
-
-    <td className="px-6 py-4 text-black">
-      <input
-        className="p-1 rounded-full border-solid border-2 border-indigo-600"
-        type="datetime-local"
-        name="datetime"
-      />
-    </td>
-
-    <td className="px-6 py-4 text-white font-semibold">{data.estado}</td>
+    <td className="px-6 py-4 text-white font-semibold">{data.Asignatura}</td>
+    <td className="px-6 py-4 text-white font-semibold">{data.Grado}</td>
+    <td className="px-6 py-4 text-white font-semibold">{data.TipoP}</td>
   </tr>
 ));
 
@@ -48,26 +28,16 @@ const TablaModifica = () => {
           <thead className="text-xs text-white uppercase bg-blue-600 dark:text-white">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Preescolar
+                Docente
               </th>
               <th scope="col" className="px-6 py-3">
-                Basica Primaria
+                Asignatura
               </th>
               <th scope="col" className="px-6 py-3">
-                Basica Secundaria
+                Grado
               </th>
               <th scope="col" className="px-6 py-3">
-                Media
-              </th>
-
-              <th scope="col" className="px-6 py-3">
-                Solicitud de Permiso desde
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Solicitud de Permiso hasta
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Estado Solicitud
+                Tipo Pendiente
               </th>
             </tr>
           </thead>
