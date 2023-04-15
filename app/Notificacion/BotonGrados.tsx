36ba6aa96f8814b2f13ccc1c0c2501b7eb846092
jhonned01 <jhonned01@gmail.com>
2023-04-15 09:40:38 -0500

    cambios auditoria

--- app/Notificacion/BotonGrados.tsx
+++ app/Notificacion/BotonGrados.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import React, { useState } from "react";
 import {
   Button,
   Dialog,
@@ -7,43 +7,19 @@ import {
   DialogFooter,
 } from "@material-tailwind/react";
 import TablaModifica from "./TablaModifica";
-
-const BotonGrados = () => {
-  const [size, setSize] = useState(null);
-  const handleOpen = (value: any) => setSize(value);
-
+const BotonGrados = ({ Grados, handleOpen, setPendientesSelected }: any) => {
   return (
     <>
-      <div className="px-2 py-2 m-1">
-        <Fragment>
-          <Button onClick={() => handleOpen("xl")} variant="gradient">
-            6-01
-          </Button>
-          <Dialog open={size === "xl"} size={size || "xl"} handler={handleOpen}>
-            <DialogHeader>Relación de los Pendientes Hallados</DialogHeader>
-            <DialogBody divider>
-              <TablaModifica />
-            </DialogBody>
-
-            <DialogFooter>
-              {/* <Button
-                variant="text"
-                color="red"
-                onClick={() => handleOpen(null)}
-                className="mr-1"
-              >
-                <span>Cancel</span>
-              </Button> */}
-              <Button
-                variant="gradient"
-                color="green"
-                onClick={() => handleOpen(null)}
-              >
-                <span>Cerrar</span>
-              </Button>
-            </DialogFooter>
-          </Dialog>
-        </Fragment>
+      <div className="px-2 py-2 m- 1">
+        <Button
+          onClick={() => {
+            handleOpen("xl");
+            setPendientesSelected(Grados.pendientes);
+          }}
+          variant="gradient"
+        >
+          {Grados.GrupoNombre}
+        </Button>
       </div>
     </>
   );
